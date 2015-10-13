964cb16d45ecd05f3bed95ca48c6b1166d022801
Steven Irvin <steven dot irvin at gmail dot com>
2015-10-12 19:47:55 -0700

    new numbers and query

---
+++ tools/query.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo_date () {
+    echo $(date)
+}
+
+private_cloud() {
+    for t in {1..5}
+    do
+        tenant_id="00${t}_tenant_abcdefgh"
+        echo $tenant_id
+        # run q 10x
+        for r in {1..10}
+        do
+            time curl --max-time 7200 'http://localhost:8777/v2/meters/instance?q.field=project_id&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=ge&q.op=le&q.type=&q.type=&q.type=&q.value='$tenant_id'&q.value=2015-10-12T18%3A30%3A00&q.value=2015-10-12T18%3A30%3A59' 1>/dev/null | wc -l; done
+    done
+}
+
+echo_date
+private_cloud
+echo_date
