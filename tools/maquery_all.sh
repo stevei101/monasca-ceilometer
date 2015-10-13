7dce22ac9d14a8b450bf1b14270871aa24d1cfed
stevei101 <steven.irvin@gmail.com>
2015-10-13 08:50:57 -0700

    m scripts

--- tools/maquery_all.sh
+++ tools/maquery_all.sh
@@ -5,7 +5,13 @@
 private_cloud() {
     /bin/bash tquery_instance.sh > tquery_instance.out &
     /bin/bash tquery_image.sh > tquery_image.out &
-    /bin/bash tquery_volume.sh tquery_volume.out &
+    /bin/bash tquery_volume.sh > tquery_volume.out &
 }
 
 private_cloud
+pid=$!
+
+# If this script is killed, kill the `cp'.
+trap "kill $pid 2> /dev/null" EXIT
+
+trap - EXIT
