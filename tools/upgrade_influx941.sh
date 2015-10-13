0fb9bb0510f92e69a04f67f44186e9e52966e1d3
stevei101 <steven.irvin@gmail.com>
2015-10-12 20:21:03 -0700

    move load, query scripts

--- tools/upgrade_influx941.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# upgrade influxdb
-wget http://influxdb.s3.amazonaws.com/influxdb_0.9.4.1_amd64.deb
-sudo service influxdb stop
-sudo dpkg -i --force-confnew influxdb_0.9.4.1_amd64.deb
-sudo service influxdb start
