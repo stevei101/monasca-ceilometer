82f02b2ed10dceb66986cf7900ade7b8c34a0de2
stevei101 <steven.irvin@gmail.com>
2015-10-13 11:33:45 -0700

    f queries

--- tools/tquery_instance.sh
+++ tools/tquery_instance.sh
@@ -10,6 +10,6 @@ do
     # run q 10x
     for r in {1..10}
     do
-        time curl -s --max-time 7200 'http://localhost:8777/v2/meters/instance?q.field=project_id&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=ge&q.op=le&q.type=&q.type=&q.type=&q.value='$tenant_id'&q.value=2015-10-12T18%3A20%3A00&q.value=2015-10-12T18%3A29%3A59' 1>/dev/null;
+        time curl -s --max-time 7200 'http://localhost:8777/v2/meters/instance?q.field=project_id&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=ge&q.op=le&q.type=&q.type=&q.type=&q.value='$tenant_id'&q.value=2015-10-13T00%3A00%3A00&q.value=2015-10-13T23%3A59%3A59' 1>/dev/null;
     done
 done
