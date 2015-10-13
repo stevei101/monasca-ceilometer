337fbc2032e484fb70dc0c982cbf5f1a3e81f003
stevei101 <steven.irvin@gmail.com>
2015-10-13 09:59:14 -0700

    cleanup

--- tools/query/query_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# all queries
-./query_instance.sh &
-./query_image.sh &
-./query_volume.sh &
--- tools/query/simple_query_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# all queries
-/bin/bash tquery_instance.sh > tquery_instance.out &
-/bin/bash tquery_image.sh > tquery_image.out &
-/bin/bash tquery_volume.sh > tquery_volume.out &
