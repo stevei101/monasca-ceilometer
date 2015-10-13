62947bbcb91ff34fa2b6301be6e2bc0f6ec26713
Steven Irvin <steven dot irvin at gmail dot com>
2015-10-13 08:23:49 -0700

    update

--- tools/maquery_all.sh
+++ tools/maquery_all.sh
@@ -6,22 +6,12 @@ echo_date () {
     echo $(date)
 }
 
-run_until () {
-    while [ kill -0 $pid0 2> /dev/null ||  kill -0 $pid1 2> /dev/null ||  kill -0 $pid2 2> /dev/null]; do
-        sleep 1s
-    done
-}
-
 private_cloud() {
-    /bin/bash tquery_instance.sh &
-    pid0=$!
-    /bin/bash tquery_instance.sh &
-    pid1=$!
-    /bin/bash tquery_instance.sh &
-    pid2=$!
+    /bin/bash tquery_instance.sh > tquery_instance.sh &
+    /bin/bash tquery_image.sh > tquery_image.sh &
+    /bin/bash tquery_volume.sh tquery_volume.sh &
 }
 
 echo_date
 private_cloud
-run_until
 echo_date
