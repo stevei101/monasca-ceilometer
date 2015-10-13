55df3975c31e574c95f006c355acb96b7f316e6d
stevei101 <steven.irvin@gmail.com>
2015-10-12 20:50:51 -0700

    m scripts

--- tools/query_all_output.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# all query
-echo $(date)
-./query_instance.sh &
-./query_image.sh &
-./query_volumee.sh &
-echo $(date)
--- tools/query_instance.sh
+++ tools/query_instance.sh
@@ -10,5 +10,5 @@ do
     # run q 10x
     for r in {1..10}
     do
-        time curl -s --max-time 7200 'http://localhost:8777/v2/meters/instance?q.field=project_id&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=ge&q.op=le&q.type=&q.type=&q.type=&q.value='$tenant_id'&q.value=2015-10-12T18%3A30%3A00&q.value=2015-10-12T18%3A30%3A59' 1>/dev/null; done
+        time curl -s --max-time 7200 'http://localhost:8777/v2/meters/instance?q.field=project_id&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=ge&q.op=le&q.type=&q.type=&q.type=&q.value='$tenant_id'&q.value=2015-10-12T18%3A20%3A00&q.value=2015-10-12T18%3A29%3A59' 1>/dev/null; done
 done
