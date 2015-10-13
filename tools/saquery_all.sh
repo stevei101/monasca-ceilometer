fc8b062c01856a09162643dcd9b7420243c968cb
Steven Irvin <steven dot irvin at gmail dot com>
2015-10-13 16:16:47 -0700

    + sequ

---
+++ tools/saquery_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# all queries
+
+private_cloud() {
+    /bin/bash tquery_instance.sh > tquery_instance.out; /bin/bash tquery_image.sh > tquery_image.out; /bin/bash tquery_volume.sh > tquery_volume.out
+}
+
+private_cloud
