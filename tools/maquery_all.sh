da8052257b7e157b1e3ff81159af979653520f61
stevei101 <steven.irvin@gmail.com>
2015-10-13 08:31:46 -0700

    m scripts

--- tools/maquery_all.sh
+++ tools/maquery_all.sh
@@ -2,16 +2,10 @@
 
 # all queries
 
-echo_date () {
-    echo $(date)
-}
-
 private_cloud() {
     /bin/bash tquery_instance.sh > tquery_instance.out &
     /bin/bash tquery_image.sh > tquery_image.out &
     /bin/bash tquery_volume.sh tquery_volume.out &
 }
 
-echo_date
 private_cloud
-echo_date
