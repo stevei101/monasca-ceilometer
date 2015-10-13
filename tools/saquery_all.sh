535cb68fef5dbcf07f5fd86dc1b9803d4dbe9511
stevei101 <steven.irvin@gmail.com>
2015-10-13 16:47:51 -0700

    update

--- tools/saquery_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# all queries
-
-private_cloud() {
-    /bin/bash tquery_instance.sh > tquery_instance.out; /bin/bash tquery_image.sh > tquery_image.out; /bin/bash tquery_volume.sh > tquery_volume.out
-}
-
-private_cloud
