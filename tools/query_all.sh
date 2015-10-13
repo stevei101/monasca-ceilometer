09e57302ce2d9ef05e1536e00f4333fa5873d2be
Steven Irvin <steven dot irvin at gmail dot com>
2015-10-12 20:29:44 -0700

    echo date

--- tools/query_all.sh
+++ tools/query_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # all query
-
-./query_instance.sh & 
-./query_image.sh & 
-./query_volumee.sh & 
+echo $(date)
+./query_instance.sh &
+./query_image.sh &
+./query_volumee.sh &
+echo $(date)
