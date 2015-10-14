1210ff2d85d50a63877e4b2cbe1e328db149f3cd
stevei101 <steven.irvin@gmail.com>
2015-10-14 16:39:53 -0700

    change dates

--- tools/tquery_volume.sh
+++ tools/tquery_volume.sh
@@ -10,6 +10,6 @@ do
     # run q 10x
     for r in {1..10}
     do
-        time curl -s --max-time 7200 'http://localhost:8777/v2/meters/volume?q.field=project_id&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=ge&q.op=le&q.type=&q.type=&q.type=&q.value='$tenant_id'&q.value=2015-10-13T00%3A00%3A00&q.value=2015-10-13T23%3A59%3A59' 1>/dev/null;
+        time curl -s --max-time 7200 'http://localhost:8777/v2/meters/volume?q.field=project_id&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=ge&q.op=le&q.type=&q.type=&q.type=&q.value='$tenant_id'&q.value=2015-10-14T00%3A00%3A00&q.value=2015-10-14T23%3A59%3A59' 1>/dev/null;
     done
 done
